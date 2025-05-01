639388fb4fb18a18f745f1bf39095068dbaca1fb
adewale-adeyelu <adewaleadeyelu17@gmail.comm>
2025-05-01 07:32:41 +0100

    main

---
+++ script.js
@@ -0,0 +1,7 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('navbar')
+    this.scrollY >= 50 ? header.classList.add('navbar-bg')
+                        :header.classList.remove('navbar-bg')
+}
+window.addEventListener('scroll', scrollHeader);
